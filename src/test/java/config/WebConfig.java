a96c0972c09f19cdafb8087b42ae11504ff2ff98
Elena Klepova <elena.klyopova@gmail.com>
2023-11-21 02:06:05 +0300

    Todoist Project

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:${env}.properties",
+        "classpath:${env}.properties"
 })
 
 public interface WebConfig extends Config{
