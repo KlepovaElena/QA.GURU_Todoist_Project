b0074077b074215d77fc2332d142f63111cef2dc
Elena Klepova <elena.klyopova@gmail.com>
2023-11-20 23:34:46 +0300

    Todoist Project

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,7 @@
 package tests;
 
-import com.codeborne.selenide.AuthenticationType;
-import com.codeborne.selenide.BearerTokenCredentials;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.sun.security.jgss.AuthorizationDataEntry;
 import config.ConfigReader;
 import config.WebConfig;
 import config.WebConfigProject;
@@ -15,8 +11,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.AuthenticationType.BASIC;
-import static com.codeborne.selenide.AuthenticationType.BEARER;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -28,20 +22,15 @@ public class TestBase {
     public static void beforeAll(){
         WebConfigProject webConfigProject = new WebConfigProject(config);
         webConfigProject.webConfig();
-        Configuration.proxyEnabled = true;
-        open("/app/today", BASIC, new BearerTokenCredentials("Authorization", "Bearer fcb77d42006db0c8fcfec8583fe4e200b06c74f5"));
-
     }
 
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.proxyEnabled = true;
-//        open(baseUrl);
-//        open("https://todoist.com/app/today", BEARER, new BearerTokenCredentials("Token", "fcb77d42006db0c8fcfec8583fe4e200b06c74f5"));
-//        open("https://todoist.com", BASIC, "", "");
-//        $("#element-0").setValue("elena.klyopova@gmail.com");
-//        $("#element-3").setValue("Elena1111!").pressEnter();
+        open(baseUrl);
+        open("/app/today");
+        $("#element-0").setValue("elena.klyopova@gmail.com");
+        $("#element-3").setValue("Elena1111!").pressEnter();
     }
 
     @AfterEach
