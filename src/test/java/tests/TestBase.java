365fe862652332397eb7be0533a1f161d8181837
Elena Klepova <elena.klyopova@gmail.com>
2023-11-21 13:20:54 +0300

    Todoist Project

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,28 +3,29 @@ package tests;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.ConfigReader;
 import config.WebConfig;
-import config.ProjectConfiguration;
+import config.WebConfigProject;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
 
-    private static final WebConfig webConfig = ConfigReader.Instance.read();
+    private static final WebConfig config = ConfigReader.Instance.read();
 
     @BeforeAll
     public static void beforeAll(){
-        ProjectConfiguration webConfigProject = new ProjectConfiguration(webConfig);
-        webConfigProject.webConfig();
-    }
+        WebConfigProject webConfigProject = new WebConfigProject(config);
+        webConfigProject.webConfig();}
 
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open(baseUrl);
         open("/app/today");
         $("#element-0").setValue("elena.klyopova@gmail.com");
         $("#element-3").setValue("Elena1111!").pressEnter();
